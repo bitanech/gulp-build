7e0c0ea61e4259abeb684e25b0ba53720128b05d
bitanech <tarasoniwe24@gmail.com>
2019-02-09 22:10:43 +0200

    set up basic build scss, img, html, fonts on watch dev added normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,60 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-let cleanCSS = require('gulp-clean-css');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const replace = require('gulp-replace');
 
-const scssToCss = () => {
+const styles = () => {
     return gulp.src('./src/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(replace('url("../img/', 'url("./img/'))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream())
 };
 
-const minifyCss = () => {
-  return gulp.src('dist/*.css')
-      .pipe(cleanCSS())
-      .pipe(gulp.dest("dist"));
-};
-
-const movePagesToDist = () => {
+const html = () => {
   return gulp.src('./src/*.html')
-      .pipe(gulp.dest("./dist"));
+      .pipe(gulp.dest("./dist"))
+      .pipe(browserSync.stream());
+};
+
+const img = () => {
+    return gulp.src('./src/img/*')
+        .pipe(gulp.dest('./dist/img'));
+};
+
+const fonts = () => {
+    return gulp.src('./src/fonts/*')
+        .pipe(gulp.dest('./dist/fonts'));
+};
+
+const cleanDist = () => {
+    return gulp.src('./dist/*')
+        .pipe(clean());
 };
 
 function defaultTask(cb) {
-    scssToCss();
-    minifyCss();
     cb();
 }
 
 function devWatch(cb) {
-    gulp.watch('./src/styles/**/*.scss', gulp.series(scssToCss, minifyCss));
-    gulp.watch('./src/*.html', gulp.series(movePagesToDist));
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch('./src/styles/**/*.scss', gulp.series(styles));
+    gulp.watch('./src/*.html', gulp.series(html));
+    gulp.watch('./src/img/*', gulp.series(img));
     cb();
 }
 exports.default = defaultTask;
